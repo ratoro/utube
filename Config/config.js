c1e8d3bf2e3efd899f43dc419e2dc724a1d06b33
Alexander Toro <ratoro0199@gmail.com>
2020-12-12 12:48:36 -0500

    okay deleting config file from repo

--- Config/config.js
+++
@@ -1,6 +0,0 @@
-let googlefitconfig = {
-    GOOGLEFIT_CLIENT_ID: "221478563217-hfl0reqicjma82s5auorrvpej6pck6gq.apps.googleusercontent.com",
-    GOOGLEFIT_CLIENT_SECRET: "0OU_S2pTG-dHN7uvVNjH0iwd"
-}
-
-module.exports={googlefitconfig}
