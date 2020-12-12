76f526754080ac4cfb9c128a340324c421e4c030
Alexander Toro <ratoro0199@gmail.com>
2020-12-12 12:43:40 -0500

    this is code i got from a youtube video that I plan on using as a basis for a side project.

---
+++ Config/config.js
@@ -0,0 +1,6 @@
+let googlefitconfig = {
+    GOOGLEFIT_CLIENT_ID: "221478563217-hfl0reqicjma82s5auorrvpej6pck6gq.apps.googleusercontent.com",
+    GOOGLEFIT_CLIENT_SECRET: "0OU_S2pTG-dHN7uvVNjH0iwd"
+}
+
+module.exports={googlefitconfig}
